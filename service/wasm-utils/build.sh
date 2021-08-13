c934e2902a8f843cf1a214b5a34a296c04aba7a0
fxg <fxg@fxgdeMacBook-Pro.local>
2021-08-13 18:14:43 +0800

    Fix:build.sh

--- service/wasm-utils/build.sh
+++ service/wasm-utils/build.sh
@@ -9,6 +9,5 @@ then
   echo "skip"
 else
   cargo build --target wasm32-unknown-unknown --release --package wasm-utils
-  wasm-opt --strip-debug -Oz target/wasm32-unknown-unknown/release/wasm_utils.wasm -o target/wasm32-unknown-unknown/release/wasm_opt.wasm
+  ic-cdk-optimizer  target/wasm32-unknown-unknown/release/wasm_utils.wasm -o target/wasm32-unknown-unknown/release/wasm_opt.wasm
 fi
-
